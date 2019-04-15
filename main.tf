9e2c454ffe0b39ef9f3fe802bb15f36ca098bcd3
venkatatmecs <ssaivenkat89@gmail.com>
2019-04-15 15:27:58 +0530

    small change

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 provider "aws"
 {
-access_key = ""
-secret_key = ""
+profile = "venkat"
+ #access_key = ""
+#secret_key = ""
 region = "us-east-1"
 }
 
-terraform {
- backend "s3" {
-    bucket = "terraformstatefile12345"
-    key    = "path/dev/terraformtfstatefile"
-    region = "us-east-1"
-    dynamodb_table = "terraformstatefile"
-  }
-}
+#terraform {
+ #backend "s3" {
+ #   bucket = "terraformstatefile12345"
+ #   key    = "path/dev/terraformtfstatefile"
+ #   region = "us-east-1"
+ #   dynamodb_table = "terraformstatefile"
+ # }
+#}
 
 resource "aws_s3_bucket" "b" {
   bucket = "venkat-s3-my-tf-test-bucket"
